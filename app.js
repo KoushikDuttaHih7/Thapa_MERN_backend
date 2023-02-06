0de9daa792029c01f6be2ac96b3fca427a495798
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-06 12:27:47 +0530

    Create a Server

--- app.js
+++ app.js
@@ -2,5 +2,25 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`Hello`);
+  res.send(`Hello from server`);
+});
+
+app.get("/about", (req, res) => {
+  res.send(`Hello from about me`);
+});
+
+app.get("/contact", (req, res) => {
+  res.send(`Hello from contact`);
+});
+
+app.get("/signin", (req, res) => {
+  res.send(`Hello from signin`);
+});
+
+app.get("/signup", (req, res) => {
+  res.send(`Hello from signup`);
+});
+
+app.listen(5000, () => {
+  console.log("Server is running at http://localhost:5000/");
 });
