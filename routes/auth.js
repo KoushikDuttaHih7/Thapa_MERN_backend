08bb7007b5c6edd86056b40e105f3e01274d5183
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-06 17:01:20 +0530

    Storing Data

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
+require("../db/conn");
+const User = require("../models/userSchema");
+
 router.get("/", (req, res) => {
   res.send(`Hello from server auth`);
 });
 
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.json({ message: req.body });
+  const { name, email, phone, work, password, cpassword } = req.body;
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ err: "ERROR" });
+  }
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ err: "already exist" });
+      }
+      const user = new User({ name, email, phone, work, password, cpassword });
+
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: "success" });
+        })
+        .catch((err) => res.status(500).json({ error: "failed" }));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
