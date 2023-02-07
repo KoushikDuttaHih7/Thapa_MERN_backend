47e68ed6c8c0305869589db9e870142e5d2b3088
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-07 12:52:15 +0530

    Create Responsive NAVBAR

---
+++ client/src/components/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div>
+      <p>WELCOME</p>
+      <h1>We are MERN Developer</h1>
+    </div>
+  );
+};
+
+export default Signup;
