dba77efd046f087a7efd38a34fbd50234c7c7741
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-07 16:55:14 +0530

    signup page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,8 +3,16 @@ import React from "react";
 const Signup = () => {
   return (
     <div>
-      <p>WELCOME</p>
-      <h1>Sign Up page</h1>
+      <section className="signup">
+        <div className="container mt-5">
+          <div className="signup-content">
+            <div className="signup-form">
+              <h2 className="form-title">Sign up</h2>
+              <form className="register-form" id="register-form"></form>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
