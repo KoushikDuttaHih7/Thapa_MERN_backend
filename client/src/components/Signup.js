7b92d4612269815dccd35d791bfa9e077b3ccf21
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-08 16:41:09 +0530

    store User Registration Form Data with React State

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,87 +1,118 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    work: "",
+    password: "",
+    cpassword: "",
+  });
+
+  let name, value;
+  const handleInputs = (e) => {
+    console.log(e);
+    name = e.target.name;
+    value = e.target.value;
+
+    setUser({ ...user, [name]: value });
+  };
+
   return (
     <div className="container">
       <form>
-        <div class="form-group">
+        <div className="form-group">
           <label for="name">Name</label>
           <input
             type="name"
-            class="form-control"
+            className="form-control"
             name="name"
             id="name"
             autoComplete="off"
+            value={user.name}
+            onChange={handleInputs}
             placeholder="Enter Name"
           />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label for="email">Email address</label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             name="email"
             id="email"
             autoComplete="off"
+            value={user.email}
+            onChange={handleInputs}
             placeholder="Enter Email"
           />
-          <small id="emailHelp" class="form-text text-muted">
+          <small id="emailHelp" className="form-text text-muted">
             We'll never share your email with anyone else.
           </small>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label for="phone">Phone</label>
           <input
             type="number"
-            class="form-control"
+            className="form-control"
             name="phone"
             id="phone"
             autoComplete="off"
+            value={user.phone}
+            onChange={handleInputs}
             placeholder="Enter Phone Number"
           />
-          <small id="emailHelp" class="form-text text-muted">
+          <small id="emailHelp" className="form-text text-muted">
             We'll never share your number with anyone else.
           </small>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label for="work">Work</label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             name="work"
             id="work"
             autoComplete="off"
+            value={user.work}
+            onChange={handleInputs}
             placeholder="Enter Your Work"
           />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label for="password">Password</label>
           <input
             type="password"
-            class="form-control"
+            className="form-control"
             name="password"
             id="password"
+            value={user.password}
+            onChange={handleInputs}
             placeholder="Password"
           />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label for="cpassword">Confirm Password</label>
           <input
             type="cpassword"
-            class="form-control"
+            className="form-control"
             name="cpassword"
             id="cpassword"
+            value={user.cpassword}
+            onChange={handleInputs}
             placeholder="Confirm Password"
           />
         </div>
         <br />
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" className="btn btn-primary">
           Submit
         </button>
       </form>
       <br />
-      <Link to="/login" className="signup-image-link">
+      <Link to="/login" classNameName="signup-image-link">
         I'm an User
       </Link>
     </div>
