f7946431c8a11a069183a0a6822f1dace29324c8
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-08 12:54:36 +0530

    created about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,11 +1,190 @@
 import React from "react";
-
+// import image from "../images/hero.jpg";
 const About = () => {
   return (
-    <div>
-      <p>WELCOME</p>
-      <h1>About Page</h1>
-    </div>
+    <>
+      <div className="container emp-profile">
+        <form action="">
+          <div className="row">
+            <div className="col-md-4">
+              {/* <img src={image} className="photo" alt="profile photo" /> */}
+            </div>
+
+            <div className="col-md-6">
+              <div className="profile-head">
+                <h5>Koushiik Dutta</h5>
+                <h6>Web Developer</h6>
+                <p className="profile-rating mt-3 mb-5">
+                  RANKINGS: <span> 1/10</span>{" "}
+                </p>
+                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                  <li className="nav-item">
+                    <a
+                      className="nav-link active"
+                      id="home-tab"
+                      data-toggle="tab"
+                      href="#home"
+                      role="tab"
+                      aria-controls="home"
+                      aria-selected="true"
+                    >
+                      About
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link active"
+                      id="profile-tab"
+                      data-toggle="tab"
+                      href="#profile"
+                      role="tab"
+                      aria-controls="profile"
+                      aria-selected="false"
+                    >
+                      Timeline
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-md-2">
+              <input
+                type="submit"
+                className="profile-edit-btn"
+                value="Edit Profile"
+                name="btnAddMore"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="profile-work">
+                <p>Links</p>
+                <a href="https://www.google.com/" target="_blank">
+                  Email
+                </a>
+                <br />
+                <a href="https://www.google.com/" target="_blank">
+                  linkedIn
+                </a>
+                <br />
+                <a href="https://www.google.com/" target="_blank">
+                  Github
+                </a>
+                <br />
+              </div>
+            </div>
+            <div className="col-md-8 pl-5 about-info">
+              <div className="tab-content profile-tab" id="myTabContent">
+                <div
+                  className="tab-pane fade show active"
+                  id="home"
+                  role="tabpanel"
+                  aria-labelledby="home-tab"
+                >
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>User ID</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>1</p>
+                    </div>
+                  </div>
+
+                  <div className="row mt-3">
+                    <div className="col-md-6">
+                      <label>Name</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Koushik</p>
+                    </div>
+                  </div>
+
+                  <div className="row mt-3">
+                    <div className="col-md-6">
+                      <label>Email</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>test.gmail.com</p>
+                    </div>
+                  </div>
+
+                  <div className="row mt-3">
+                    <div className="col-md-6">
+                      <label>Phone</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>1234567890</p>
+                    </div>
+                  </div>
+
+                  <div className="row mt-3">
+                    <div className="col-md-6">
+                      <label>Profession</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Web developer</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="tab-pane fade show "
+                  id="profile"
+                  role="tabpanel"
+                  aria-labelledby="profile-tab"
+                >
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label> Experiance</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Junior</p>
+                    </div>
+                  </div>
+
+                  <div className="row mt-3">
+                    <div className="col-md-6">
+                      <label>Hourly Rate</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Number</p>
+                    </div>
+                  </div>
+
+                  <div className="row mt-3">
+                    <div className="col-md-6">
+                      <label>Total Projects</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>10</p>
+                    </div>
+                  </div>
+
+                  <div className="row mt-3">
+                    <div className="col-md-6">
+                      <label>English Level</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Expert</p>
+                    </div>
+                  </div>
+
+                  <div className="row mt-3">
+                    <div className="col-md-6">
+                      <label>Availability</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>1 Months</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
