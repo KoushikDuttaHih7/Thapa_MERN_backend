56f8c00603ae3374a851803c30af25d007889560
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-09 16:53:14 +0530

    Home Page dynamic

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [userName, setUserName] = useState("");
+  const [show, setShow] = useState(false);
+
+  const userHomePage = async () => {
+    try {
+      const res = await fetch("/getdata", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+      setUserName(data.name);
+
+      setShow(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    userHomePage();
+  });
   return (
     <div className="home">
       <div className="home-div h-100 d-flex flex-column justify-content-center align-items-center">
@@ -12,7 +35,8 @@ const Home = () => {
             WELCOME
           </p>
         </div>
-        <h1>We are the MERN Developer</h1>
+        <h1>{userName}</h1>
+        <h2>{show ? "Happy to see you Here " : "We are the MERN Developer"}</h2>
       </div>
     </div>
   );
